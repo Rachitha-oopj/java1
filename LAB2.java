aefff462cdcffec70409d9d2c13f5c3946371ff0
Rachitha-oopj <rachitha.23cs109@sode-edu.in>
2024-10-26 09:19:12 +0530

    Add files via upload

---
+++ LAB2.java
@@ -0,0 +1,9 @@
+public class LAB2{
+    public static void main(String[] args){
+        int[] ar=new int[]{3,4,5,6,7};
+        System.out.println("Elements of given array:");
+        for(int i=0;i<ar.length;i++){
+            System.out.println(ar[i]+" ");  
+        }
+    }
+}
