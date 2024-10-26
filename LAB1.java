714fd0402ee462db8bd7cbe59e0f48a00235ca65
Rachitha-oopj <rachitha.23cs109@sode-edu.in>
2024-10-26 09:18:42 +0530

    Add files via upload

---
+++ LAB1.java
@@ -0,0 +1,8 @@
+public class LAB1{
+    public static void main(String[] args){
+        double tempC,tempF;
+        tempC=100;
+        tempF=tempC*(9.0/5.0)+32;
+        System.out.println("Temperature is:"+tempF);
+    } 
+}
