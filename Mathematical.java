a358db85e7003da02a09347265d174ad06af1041
Rachitha-oopj <rachitha.23cs109@sode-edu.in>
2024-10-26 10:39:47 +0530

    Add files via upload

---
+++ Mathematical.java
@@ -0,0 +1,10 @@
+public class Mathematical{
+    public static void main(String[] args)
+    {
+        int n=3;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " * " + i + " = " + n * i);
+                               
+        }
+    }
+}
