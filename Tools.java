4908bced5ce109789f8c879eac091128d1ea2eb6
Rachitha-oopj <rachitha.23cs109@sode-edu.in>
2024-11-14 10:46:15 +0530

    Tools.java

--- Tools.java
+++ Tools.java
@@ -8,40 +8,40 @@ public class Tools{
         System.out.println();
     }
     public void even(int n){
-        for(int i=0;i<=n;i+=2)
+        for(int i=0;i<=n;i++)
         {
-            System.out.print(i+" ");
+            System.out.print(2*i+" ");
         }
         System.out.println();
     }
     public void odd(int n){
-        for(int i=1;i<=n;i+=2)
+        for(int i=1;i<=n;i++)
         {
-            System.out.print(i+" ");
+            System.out.print(2*i+1+" ");
         }
         System.out.println();
     }
     public void mult3(int n)
     {
-        for(int i=0;i<=n;i+=3)
+        for(int i=0;i<=n;i++)
         {
-            System.out.print(i+" ");
+            System.out.print(3*i+" ");
         }
         System.out.println();
     }
     public void mult4(int n)
     {
-        for(int i=0;i<=n;i+=4)
+        for(int i=0;i<=n;i++)
         {
-            System.out.print(i+" ");
+            System.out.print(4*i+" ");
         }
         System.out.println();
     }
     public void mult5(int n)
     {
-        for(int i=0;i<=n;i+=5)
+        for(int i=0;i<=n;i++)
         {
-            System.out.print(i+" ");
+            System.out.print(5*i+" ");
         }
         System.out.println();
     }
